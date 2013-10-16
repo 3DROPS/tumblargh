2d0abe0d87547b8b75dfdb2741798d58c87aac41
Eric Kreutzer <eric@airkrft.com>
2013-10-16 17:25:38 -0600

    More tags support

--- lib/tumblargh/renderer/blocks/posts.rb
+++ lib/tumblargh/renderer/blocks/posts.rb
@@ -62,7 +62,23 @@ module Tumblargh
         end
 
         def post_author_portrait_url(size)
-          return "http://placekitten.com/#{size}/#{size}"
+          "http://placekitten.com/#{size}/#{size}"
+        end
+
+        def post_author_name
+          "Daniel Day-Lewis"
+        end
+
+        def post_author_url
+          "http://fban.tumblr.com"
+        end
+
+        def reblog_button
+          '<a href="http://www.tumblr.com/reblog/64239114145/8jhxyfBJ" class="reblog_button" style="display: block;width:20px;height: 20px;display: block;"><svg width="100%" height="100%" viewBox="0 0 537 512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ccc"><path d="M 98.893,177.139c0.00-7.462, 4.826-12.275, 12.288-12.275L 405.12,164.864 l0.00,83.469 l 118.72-120.947L 405.12,8.678l0.00,81.51 L 49.382,90.189 c-15.206,0.00-27.648,12.429-27.648,27.648l0.00,171.814 l 77.146-71.603L 98.88,177.139 z M 438.874,332.646c0.00,7.45-4.826,12.275-12.275,12.275L 123.75,344.922 l0.00-83.469 l-116.506,120.922l 116.506,120.947l0.00-81.498 l 356.864,0.00 c 15.206,0.00, 27.648-12.454, 27.648-27.648L 508.262,220.134 l-69.402,71.59L 438.861,332.646 z"></path></svg></a>'
+        end
+
+        def like_button
+          '<div class="like_button" data-post-id="64239114145" id="like_button_64239114145"><iframe id="like_iframe_64239114145" src="http://assets.tumblr.com/assets/html/like_iframe.html?_v=f55b209dcbe59a18a8171512f8af09d1#name=threedrops&amp;post_id=64239114145&amp;rk=8jhxyfBJ" scrolling="no" width="20" height="20" frameborder="0" class="like_toggle" allowtransparency="true"></iframe></div>'
         end
 
         def render
